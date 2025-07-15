5d12e9e7330d2be3687ab1cad75f99e15746e960
Adam Thompson <aleethompson09@gmail.com>
2025-07-15 14:32:41 -0400

    Update game and add implementation

---
+++ src/game.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+#include "game.hpp"
+#include "game_object.hpp"
+#include "sprite_renderer.hpp"
+#include "sprite.hpp"
+#include "input_handler.hpp"
+#include "player.hpp"
+#include "enemy.hpp"
+
+namespace pathogen
+{
+	Game::~Game()
+	{
+		for (GameObject* obj : gameObjects)
+			delete obj;
+		gameObjects.clear();
+
+		if (spriteRenderer)
+		{
+			spriteRenderer->shutdown();
+			delete spriteRenderer;
+			spriteRenderer = nullptr;
+		}
+	}
+
+	Player* setupPlayer()
+	{
+		auto* player = new Player();
+		player->sprite.texPath = "Pathogen.png";
+		
+		return player;
+	}
+
+	void Game::init()
+	{
+		spriteRenderer = new SpriteRenderer();
+		spriteRenderer->init();
+		auto* player = setupPlayer();
+
+		addGameObject(player);
+
+		auto* testEnemy = new Enemy();
+		testEnemy->sprite.texPath = "Pathogen.png";
+		testEnemy->setPlayer(player);
+
+		addGameObject(testEnemy);
+	}
+
+	void Game::setScreenDim(int width, int height)
+	{
+		if (!spriteRenderer)
+		{
+			std::cerr << "ERROR: Sprite Renderer used before initialization, Initialize Game prior to use." << std::endl;
+			return;
+		}
+
+		spriteRenderer->setWorldOrigin(width / 2.0f, height / 2.0f);
+		spriteRenderer->setScreenDim(width, height);
+	}
+
+	// Adds a Game Object and generates a new id if it doesn't have one.
+	void Game::addGameObject(GameObject* gameObject)
+	{
+		if (gameObject->instanceID == 0)
+		{
+			gameObject->instanceID = getNextID();
+		}
+		for (size_t i = 0; i < gameObjects.size(); ++i)
+		{
+			if (gameObjects[i]->instanceID == gameObject->instanceID)
+			{
+				return;
+			}
+		}
+
+		gameObjects.push_back(gameObject);
+	}
+
+	void Game::tick(float dt)
+	{
+		time += dt;
+		
+		for (auto& obj : gameObjects)
+		{
+			obj->update(dt);
+
+			if (obj->getType() == ObjectType::Player)
+			{
+				camera.x = obj->sprite.x;
+				camera.y = obj->sprite.y;
+			}
+		}
+	}
+
+	void Game::draw()
+	{
+		for (auto& obj : gameObjects)
+		{
+			obj->draw(spriteRenderer, camera.x, camera.y);
+		}
+	}
+
+}
--- src/game.hpp
+++ src/game.hpp
@@ -4,21 +4,37 @@
 
 namespace pathogen
 {
-    struct Sprite;
+    class GameObject;
+    class SpriteRenderer;
 
     struct Camera
     {
-        float x, y;
+        float x = 0.0f, y = 0.0f;
         float zoom = 1.0f;
     };
 
     struct Game
     {
-        std::vector<Sprite> sprites;
-        Camera camera;
-        float time;
+        ~Game();
 
-        void tick();
+        void init();
+
+        void setScreenDim(int width, int height);
+
+        void addGameObject(GameObject* gameObject);
+
+        void tick(float dt);
         void draw();
+    
+        std::vector<GameObject*> gameObjects;
+        Camera camera;
+        SpriteRenderer* spriteRenderer = nullptr;
+        float time = 0.0f;
+        float deltaTime = 0.0f;
+
+    private:
+        inline static int nextID = 0;
+        inline int getNextID() { return ++nextID; }
+
     };
 }
